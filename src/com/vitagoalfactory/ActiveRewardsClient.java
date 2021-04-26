4e69bc9af7eb54959ab0112ab9c685f935ce5efb
FAKO1 <fakop@discovery.co,za>
2021-04-26 13:58:23 +0200

    Init

---
+++ src/com/vitagoalfactory/ActiveRewardsClient.java
@@ -0,0 +1,13 @@
+package com.vitagoalfactory;
+
+public class ActiveRewardsClient {
+
+    /**
+     * Implement a goal factory resembling that of Vitality Active Rewards
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
