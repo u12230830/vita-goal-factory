99eb59031a1dfedc69a2f5a63af3faabdd699754
FAKO1 <fakop@discovery.co,za>
2021-04-28 06:44:13 +0200

    Init

---
+++ src/com/vitagoalfactory/Goal.java
@@ -0,0 +1,11 @@
+package com.vitagoalfactory;
+
+import java.util.Date;
+
+public abstract class Goal {
+    private String description;
+    private Date startDate;
+    private Date endDate;
+    private boolean achieved;
+    private String memberName;
+}
